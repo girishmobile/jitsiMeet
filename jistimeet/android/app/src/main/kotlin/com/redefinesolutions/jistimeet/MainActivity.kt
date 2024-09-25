8f9fe68e2e98d1adaf6f785726cb75765186302e
girishmobile <macmini01062019@gmail.com>
2024-09-25 14:50:09 +0530

    code

---
+++ jistimeet/android/app/src/main/kotlin/com/redefinesolutions/jistimeet/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.redefinesolutions.jistimeet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ jistimeet/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:jistimeet/page/main_page.dart';
+import 'package:jistimeet/page/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Jitsi Meet',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
+
